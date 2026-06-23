fede0f2409412e72a63ca70fb6cc6459d0ed8400
Rajwardhan1807 <rajvardhan1807@gmail.com>
2026-06-24 00:09:50 +0530

    problem 21-repaired

--- 21_Mege_2_sort_list.cpp
+++ 21_Mege_2_sort_list.cpp
@@ -9,51 +9,62 @@
  * };
  */
 class Solution {
-    ListNode * solve(ListNode * l1, ListNode * l2){
-        if(l1->next == NULL){
-            l1->next = l2;
-            return NULL;
-        }
-        ListNode * curr1 = l1;
-        ListNode * next1 = curr1->next;
-        
-        ListNode * curr2 = l2;
-        ListNode * next2 = curr2->next;
+private:
+    ListNode* solve(ListNode* l1, ListNode* l2) {
 
-        while(next1 != NULL && curr2 != NULL){
-            if((curr2->val >= curr1->val) && (curr2->val <= next1->val)){
-                curr1->next = curr2;
+        // If first list has only one node
+        if (l1->next == NULL) {
+            l1->next = l2;
+            return l1;
+        }
+
+        ListNode* curr1 = l1;
+        ListNode* next1 = l1->next;
+
+        ListNode* curr2 = l2;
+        ListNode* next2 = NULL;
+
+        while (next1 != NULL && curr2 != NULL) {
+
+            if (curr2->val >= curr1->val &&
+                curr2->val <= next1->val) {
+
+                // Insert curr2 between curr1 and next1
                 next2 = curr2->next;
+
+                curr1->next = curr2;
                 curr2->next = next1;
+
                 curr1 = curr2;
                 curr2 = next2;
             }
-
-            else{
+            else {
                 curr1 = next1;
                 next1 = next1->next;
 
-                if(next1 == NULL){
+                // Reached end of first list
+                if (next1 == NULL) {
                     curr1->next = curr2;
                     return l1;
                 }
             }
         }
+
         return l1;
     }
+
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1 == NULL){
+
+        if (list1 == NULL)
             return list2;
-        }
-        if(list2 == NULL){
+
+        if (list2 == NULL)
             return list1;
-        }
-        if(list1->val <= list2->val){
-            return solve(list1,list2);
-        }
-        else{
-            return solve(list2,list1);
-        }
+
+        if (list1->val <= list2->val)
+            return solve(list1, list2);
+        else
+            return solve(list2, list1);
     }
 };
